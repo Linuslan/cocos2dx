d3629dd3eee4ca289c1bc795f1636284bcce400c
Linuslan <linuslan@sina.cn>
2018-06-08 19:51:47 +0800

    111

--- careergame/careergame/Classes/layer/home/HomeLayer.cpp
+++ careergame/careergame/Classes/layer/home/HomeLayer.cpp
@@ -14,8 +14,10 @@ bool HomeLayer::init() {
         bg->setPosition(winSize/2);
         this->addChild(bg);
         Sprite* computer = Sprite::create("test/home_computer.png");
-        computer->setPosition(Vec2(winSize.width*0.026, winSize.height*0.3));
+        computer->setPosition(Vec2(winSize.width*0.026, winSize.height*0.35));
         this->addChild(computer);
+        Sprite* chair = Sprite::create("test/home_chair.png");
+        chair->setPosition(Vec2(winSize.width*0.026, winSize.height*0.35));
     } catch(std::exception& ex) {
         log("初始化家框架异常，%s", ex.what());
     }
