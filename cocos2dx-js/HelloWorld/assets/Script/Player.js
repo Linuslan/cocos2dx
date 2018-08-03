60a94d8eb1337bd3c950d89c940efb7f0ce5bace
Linuslan <linuslan@sina.cn>
2018-08-03 15:27:13 +0800

    111

---
+++ cocos2dx-js/HelloWorld/assets/Script/Player.js
@@ -0,0 +1,70 @@
+// Learn cc.Class:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/class.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/class/index.html
+// Learn Attribute:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/reference/attributes.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/reference/attributes/index.html
+// Learn life-cycle callbacks:
+//  - [Chinese] http://www.cocos.com/docs/creator/scripting/life-cycle-callbacks.html
+//  - [English] http://www.cocos2d-x.org/docs/editors_and_tools/creator-chapters/scripting/life-cycle-callbacks/index.html
+
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        jumpHeight: 0,
+        jumpDuration: 0,
+        maxMoveSpeed: 0,
+        accel: 0
+    },
+
+    setJumpAction: function() {
+        var jumpUp = cc.moveBy(this.jumpDuration, cc.p(0, this.jumpHeight)).easing(cc.easeCubicActionOut());
+        var jumpDown = cc.moveBy(this.jumpDuration, cc.p(0, -this.jumpHeight)).easing(cc.easeCubicActionIn());
+        return cc.repeatForever(cc.sequence(jumpUp, jumpDown));
+    },
+    setInputControl: function() {
+        var self = this;
+        cc.eventManager.addListener({
+            event: cc.EventListener.KEYBOARD,
+            onKeyPressed: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a: {
+                        self.accLeft = true;
+                        self.accRight = false;
+                    } break;
+                    case cc.KEY.d: {
+                        self.accLeft = false;
+                        self.accRight = true;
+                    } break;
+                }
+            },
+            onKeyReleased: function(keyCode, event) {
+                switch(keyCode) {
+                    case cc.KEY.a: {
+                        self.accLeft = false;
+                    } break;
+                    case cc.KEY.d: {
+                        self.accRight = false;
+                    } break;
+                }
+            }
+        }, self.node);
+    },
+    // LIFE-CYCLE CALLBACKS:
+
+    onLoad: function() {
+        this.jumpAction = this.setJumpAction();
+        this.node.runAction(this.jumpAction);
+        this.accRight = false;
+        this.accLeft = false;
+        this.xSpeed = 0;
+        this.setInputControl();
+    },
+
+    start () {
+
+    },
+
+    // update (dt) {},
+});
