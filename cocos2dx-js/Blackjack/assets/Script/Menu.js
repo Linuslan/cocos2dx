05528d396d20e0000c1de8ce34060cd982f245e4
Linuslan <linuslan@sina.cn>
2018-09-13 00:11:54 +0800

    111

---
+++ cocos2dx-js/Blackjack/assets/Script/Menu.js
@@ -0,0 +1,36 @@
+cc.Class({
+    extends: cc.Component,
+
+    properties: {
+        
+    },
+
+    // use this for initialization
+    onLoad: function () {
+        cc.director.preloadScene("Game", function() {
+            console.log("预加载游戏场景");
+        });
+        var quickStartBtn = this.node.getChildByName("quickStartBtn");
+        if(quickStartBtn) {
+            quickStartBtn.on(cc.Node.EventType.TOUCH_START, function(event) {
+                console.log("按下快速开始按钮");
+                var btn = event.target;
+                btn.runAction(cc.scaleTo(0.1, 0.85));
+            });
+            quickStartBtn.on(cc.Node.EventType.TOUCH_END, function(event) {
+                console.log("按下快速开始按钮结束");
+                var btn = event.target;
+                var action = cc.sequence(cc.scaleTo(0.1, 1), cc.callFunc(function() {
+                    console.log("开始执行动作回调");
+                    cc.director.loadScene("Game");
+                }));
+                btn.runAction(action);
+            });
+        }
+    },
+
+    // called every frame, uncomment this function to activate update callback
+    // update: function (dt) {
+
+    // },
+});
